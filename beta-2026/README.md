2df48cc862945b2b0073221b1c306c64a5d7cebf
Denis Ah-Kang <1696128+deniak@users.noreply.github.com>
2026-03-11 07:24:12 +0400

    specs for A/B testing (#37)
    * beta test: apply new design to file-system-access and scheduling-apis
    
    * update guidance based on metadata
    
    * Apply suggestions from code review
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>
    
    * Update spec-mockups/file-system-access/b.html
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>
    
    * include dontcallmedom's suggestions into scheduling APIs
    
    * remove beta box and add sentence in the SotD
    
    * add thumbs down to indicate negative signals
    
    * integrate baseline-status
    
    * remove dark mode specifics
    
    * Apply suggestions from code review
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>
    
    * keep 'Community support' only if there's relevant information
    
    * remove baseline-status
    
    * move beta specs into a different folder
    
    ---------
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

---
+++ beta-2026/README.md
@@ -0,0 +1,6 @@
+# CG specifications lifecycle beta
+
+New design on 2 specifications [file-system-access](https://wicg.github.io/file-system-access/) and [scheduling-apis](https://wicg.github.io/scheduling-apis/).
+
+* file-system-access: [old design](https://w3c.github.io/cg-program/beta-2026/file-system-access/a.html) / [new design](https://w3c.github.io/cg-program/beta-2026/file-system-access/b.html)
+* scheduling-apis: [old design](https://w3c.github.io/cg-program/beta-2026/scheduling-apis/a.html) / [new design](https://w3c.github.io/cg-program/beta-2026/scheduling-apis/b.html)
---
+++ beta-2026/assets/cg-spec.js
@@ -0,0 +1,13 @@
+// Get the announcement Bar
+const announcementBar = document.getElementById("announcement-bar");
+// Get the dismiss button
+const announcementDismiss = document.getElementById("announcement-dismiss");
+
+// On page load the announcement is sticky, so we add scroll padding to the top of the HTML element, so anchor links won't get hidden behind the announcemnet bar.
+html.style.scrollPaddingTop = "" + (announcementBar.offsetHeight + 48) + "px";
+
+// Onclick change the aria-pressed value to true, to notify assistive tech of the change and change scrollPaddingTop to a default value
+function stickyAnnouncementDismiss() {
+  announcementDismiss.ariaPressed = "true"; // set to true
+  html.style.scrollPaddingTop = "3rem"; // remove the scrollPaddingTop allowance for the announcement, by reverting to the default value
+}
