ec5a9fcc33db76ec79baf839a0125864332c181e
Denis Ah-Kang <denis@w3.org>
2026-03-19 18:44:30 +0400

    fix scroll padding

--- beta-2026/assets/cg-spec.js
+++ beta-2026/assets/cg-spec.js
@@ -4,10 +4,10 @@ const announcementBar = document.getElementById("announcement-bar");
 const announcementDismiss = document.getElementById("announcement-dismiss");
 
 // On page load the announcement is sticky, so we add scroll padding to the top of the HTML element, so anchor links won't get hidden behind the announcemnet bar.
-document.body.style.scrollPaddingTop = "" + (announcementBar.offsetHeight + 48) + "px";
+document.documentElement.style.scrollPaddingTop = "" + (announcementBar.offsetHeight + 48) + "px";
 
 // Onclick change the aria-pressed value to true, to notify assistive tech of the change and change scrollPaddingTop to a default value
 function stickyAnnouncementDismiss() {
   announcementDismiss.ariaPressed = "true"; // set to true
-  document.body.style.scrollPaddingTop = "3rem"; // remove the scrollPaddingTop allowance for the announcement, by reverting to the default value
+  document.documentElement.style.scrollPaddingTop = "3rem"; // remove the scrollPaddingTop allowance for the announcement, by reverting to the default value
 }
